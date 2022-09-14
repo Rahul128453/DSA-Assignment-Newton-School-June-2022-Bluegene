f41b02fa286bb084683cbc154fc8319e5bb99ef0
Saurabh <saurabhsyadav1999@.com>
2022-09-14 12:04:14 +0530

    newton first assignment updated

---
+++ Basics of programming,input output/Area of Square.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+class Main {
+static Scanner in = new Scanner(System.in);
+	public static void main(String args[]){
+		int length = in.nextInt();
+		int area=length*length;
+		System.out.println(area) ;
+	}
+}

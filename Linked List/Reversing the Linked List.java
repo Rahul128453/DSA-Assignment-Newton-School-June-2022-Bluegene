a9bf6978db9dc557a495019c19099c611da75a75
Saurabh <saurabhsyadav1999@.com>
2022-09-14 10:46:16 +0530

    LL assignment Updated

---
+++ Linked List/Reversing the Linked List.java
@@ -0,0 +1,16 @@
+
+public static Node ReverseLinkedList(Node head) {
+
+    Node prev = null; 
+    Node current = head; 
+    Node next = null; 
+    while (current != null) { 
+        next = current.next; 
+        current.next = prev; 
+        prev = current; 
+        current = next; 
+    } 
+    head = prev; 
+    return head; 
+    
+}
